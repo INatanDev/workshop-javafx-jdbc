b286daf18afb12a94f6608938dd90a0840cef0a1
INatanDev <natan.ferreira21@hotmail.com>
2023-07-05 22:34:20 -0300

    Department ComboBox

--- src/gui/DepartmentListContoller.java
+++ src/gui/DepartmentListContoller.java
@@ -111,6 +111,7 @@ public class DepartmentListContoller implements Initializable, DataChangeListene
 			dialogStage.showAndWait();
 
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
--- src/gui/SellerListContoller.java
+++ src/gui/SellerListContoller.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerListContoller implements Initializable, DataChangeListener {
@@ -113,7 +114,8 @@ public class SellerListContoller implements Initializable, DataChangeListener {
 
 			SellerFormController controller = loader.getController();
 			controller.setSeller(obj);
-			controller.setSellerService(new SellerService());
+			controller.setServices(new SellerService(), new DepartmentService());
+			controller.loadAssociatedObjects();
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
 
@@ -126,6 +128,7 @@ public class SellerListContoller implements Initializable, DataChangeListener {
 			dialogStage.showAndWait();
 
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
